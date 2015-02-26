4c280581aa4da507033090f8ae12eeabeda359c9
Oliver <oliverbarnum32@gmail.com>
2015-02-26 01:31:22 -0500

    It's beautiful

--- abc.py
+++ abc.py
@@ -8,18 +8,23 @@ np.random.seed()
 
 culture_limit = 1.0
 distance = culture_limit * 0.6
-number_clusters = 40
-number_per_cluster = 1
+number_clusters = 30
+number_per_cluster = 5
 spread = False
 
-north_color = (1,0,0)
-east_color = (0.2,0.2,0.6)
-south_color = (0.2,0.6,0)
-west_color = (1,1,0)
+worldliness_max = 1
+
+radian_shock = True
+magnitude_shock = True
+
+north_color = (1,1,0)
+east_color = (1,0.5,0)
+south_color = (0.545/255,0.1254,0.73)
+west_color = (0,0.7,0.83921)
 
 people = list()
 
-size = 400
+max_size = 3000
 alph = 1
 linewidth = 0
 
@@ -35,16 +40,22 @@ class Person:
         self.y = y
         
     def draw(self):
-        self.size = size #* self.worldliness
+        if(self.worldliness >worldliness_max):
+            self.worldliness = worldliness_max
+        elif(self.worldliness <0):
+            self.worldliness = 0
+            
+        self.size = max_size *  (0.1 + (0.9 * self.worldliness/worldliness_max))
         
-        if(self.x>culture_limit): 
-            self.x =culture_limit
-        if(self.x<-culture_limit): 
-            self.x =-culture_limit
-        if(self.y>culture_limit): 
-            self.y =culture_limit
-        if(self.y<-culture_limit): 
-            self.y =-culture_limit
+        
+        while(self.x>culture_limit): 
+            self.x = self.x-culture_limit
+        while(self.x<-culture_limit): 
+            self.x =self.x+culture_limit
+        while(self.y>culture_limit): 
+            self.y =self.y-culture_limit
+        while(self.y<-culture_limit): 
+            self.y =self.y+culture_limit
         
         colors = [0,0,0]
         if(self.x>0 and self.y>0):
@@ -76,22 +87,95 @@ class Person:
    
 def interact(a,b):
     #cd../documents/github/culturecolors
+    
     distance = math.fabs(math.sqrt(((a.x-b.x)*(a.x-b.x)) + ((a.y-b.y)*(a.y-b.y))))
+    
     
     a_magnitude = math.sqrt((a.x*a.x) + (a.y*a.y))
     b_magnitude = math.sqrt((b.x*b.x) + (b.y*b.y))
     
-    a_angle = math.atan(a.y/a.x)
-    b_angle = math.atan(b.y/b.x)
+    a_angle = math.atan2(a.y,a.x)
+    b_angle = math.atan2(b.y,b.x)
+      
+   
+    if(magnitude_shock):
+        value = 50.0
+        movement_value = 10.0
+        if(distance > (culture_limit*2) * 0.8):
+            #Shockingly different
+            #if ^^ distance  
+            #  worldliness vv  
+            #  go away from centre barely
+            #  go away from one another greatly
+            a.worldliness = a.worldliness-(2/movement_value)
+            b.worldliness = b.worldliness-(2/movement_value)
+            
+            a_magnitude= a_magnitude+(1/value)
+            b_magnitude= a_magnitude+(1/value)
+            
+        elif(distance > (culture_limit*2) * 0.5):
+            #You learn a great deal from this person
+            #if ^  distance
+            #  worldliness ^^
+            #  go towards centre greatly
+            #  go away from one another slightly
+            
+            a.worldliness = a.worldliness+(2/movement_value)
+            b.worldliness = b.worldliness+(2/movement_value)
+            
+            a_magnitude= a_magnitude-(2/value)
+            b_magnitude= a_magnitude-(2/value)
+            
+        elif(distance > (culture_limit*2) * 0.20):
+            #You learn a bit from this person
+            #if v  distance
+            #  worldliness v   
+            #  go towards centre barely
+            #  go towards one another slightly
+            
+            a.worldliness = a.worldliness-(1/movement_value)
+            b.worldliness = b.worldliness-(1/movement_value)
+            
+            a_magnitude= a_magnitude-(1/value)
+            b_magnitude= a_magnitude-(1/value)
+            
+        else:
+            #Someone very close to you, you isolate yourselves
+            #if vv 
+            #  distance  worldliness
+            #  go away from centre greatly
+            #  go towards one another greatly
+             
+            a.worldliness = a.worldliness+(1/movement_value)
+            b.worldliness = b.worldliness+(1/movement_value)
+            
+            a_magnitude= a_magnitude+(2/value)
+            b_magnitude= a_magnitude+(2/value)
+        
+    #in any case move their angles closer together
+    if(radian_shock):
+        amount = (2*math.pi)/360
+        if(a_angle>b_angle):
+            if((2*math.pi) - a_angle + b_angle < a_angle-b_angle):
+                a_angle = a_angle + amount
+                b_angle = b_angle - amount
+            else:
+                a_angle = a_angle - amount
+                b_angle = b_angle + amount
+        else:
+            if((2*math.pi) - b_angle + a_angle < b_angle-a_angle):
+                b_angle = b_angle + amount
+                a_angle = a_angle - amount
+            else:
+                b_angle = b_angle - amount
+                a_angle = a_angle + amount
     
+    #Update new positions     
     a.x = math.cos(a_angle) * a_magnitude
     a.y = math.sin(a_angle) * a_magnitude
-        
+    
     b.x = math.cos(b_angle) * b_magnitude
-    b.y = math.sin(b_angle) * b_magnitude
-    
-    
-    #in any case move their angles closer together
+    b.y = math.sin(b_angle) * b_magnitude  
 
 def animate(i):
     plt.clf()
@@ -113,7 +197,7 @@ def animate(i):
     
     
 #  Creating clusters    
-for c in range(0, 40):
+for c in range(0, number_clusters):
     angle = (2 * math.pi) / number_clusters * c 
     
     cluster_x = math.cos(angle) * distance
@@ -132,5 +216,5 @@ fig = plt.figure()
 fig.set_size_inches(10,10)
 plt.grid(True)
 
-ani = animation.FuncAnimation(fig,animate,interval=300)
+ani = animation.FuncAnimation(fig,animate,interval=600)
 plt.show()
